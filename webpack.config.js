fa13bedf6014c41862e8730d831feff70e6a7938
thibaut.vanspaandonck <thibaut@urge2code.com>
2016-05-31 09:49:31 +0200

    Added Babel Webpack loader + ES2015 and React presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   entry: './app/app.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel?presets[]=es2015&presets[]=react']
+      }
+    ],
+  },
   output: {
     path: './dist',
     filename: 'app.js'
