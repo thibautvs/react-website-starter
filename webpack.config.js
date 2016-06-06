83577c3ae0c00a78c525ff728047e487e727f231
thibaut.vanspaandonck <thibaut@urge2code.com>
2016-06-06 09:18:12 +0200

    Configured CSS hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ var webpack = require('webpack');
 var path = require('path');
 var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var production = process.env.NODE_ENV === 'production';
 
 module.exports = {
   context: path.resolve(__dirname, 'app'),
   entry: {
-    html: './index.html',
+    html: './index-' + process.env.NODE_ENV + '.html',
     javascript: './app.js',
     vendor: ['react', 'react-dom']
   },
   module: {
     loaders: [
-      { test: /\.html$/, loader: 'file?name=[name].[ext]' },
-      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!sass!postcss') },
+      { test: /\.html$/, loader: 'file?name=index.html' },
+      { test: /\.scss$/, loader: production ? ExtractTextPlugin.extract('style', 'css!sass!postcss') : 'style!css!sass!postcss' },
       { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel?presets[]=es2015&presets[]=react'] },
       { test: /assets/, loader: 'file?name=[path][name].[ext]' },
       { test: require.resolve('react'), loader: 'expose?React' }
@@ -21,6 +22,7 @@ module.exports = {
   },
   postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
   plugins: [
+    new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV) } }),
     new ExtractTextPlugin('react-website-starter.css'),
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
   ],
