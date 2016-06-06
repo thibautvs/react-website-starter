8bc011f8c97b450e6d30880145255eede7640652
thibaut.vanspaandonck <thibaut@urge2code.com>
2016-06-06 15:22:17 +0200

    Configured source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var production = process.env.NODE_ENV === 'production';
+var isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   context: path.resolve(__dirname, 'app'),
@@ -14,8 +14,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.html$/, loader: 'file?name=index.html' },
-      { test: /\.scss$/, loader: production ? ExtractTextPlugin.extract('style', 'css!sass!postcss') : 'style!css!sass!postcss' },
-      { test: /\.js$/, exclude: /node_modules/, loader: (production ? '' : 'react-hot!') + 'babel?presets[]=es2015&presets[]=react' },
+      { test: /\.scss$/, loader: isProd ? ExtractTextPlugin.extract('style', 'css!sass!postcss') : 'style!css?sourceMap!sass?sourceMap!postcss' },
+      { test: /\.js$/, exclude: /node_modules/, loader: (isProd ? '' : 'react-hot!') + 'babel?presets[]=es2015&presets[]=react' },
       { test: /assets/, loader: 'file?name=[path][name].[ext]' },
       { test: require.resolve('react'), loader: 'expose?React' }
     ],
@@ -25,7 +25,7 @@ module.exports = {
     new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV) } }),
     new ExtractTextPlugin('react-website-starter.css'),
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
-    production ? new webpack.optimize.UglifyJsPlugin() : function() {}
+    isProd ? new webpack.optimize.UglifyJsPlugin() : function() {}
   ],
   resolveLoader: {
     root: path.resolve(__dirname, 'node_modules')
@@ -34,6 +34,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'react-website-starter.js'
   },
+  devTool: 'eval-source-map',
   devServer: {
     port: 8000
   }
