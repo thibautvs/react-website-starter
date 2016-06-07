3ff98600ae07b8346c8a0074b2fc7ead5efb7795
thibaut.vanspaandonck <thibaut@urge2code.com>
2016-06-07 08:56:59 +0200

    Bundle fingerprinting now done in production mode only

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,9 @@ module.exports = {
   postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
   plugins: [
     new HtmlWebpackPlugin({ template: './index.tmpl.html' }),
-    new ExtractTextPlugin('react-website-starter-[hash].css'),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor-[hash].js'),
+    new ExtractTextPlugin('react-website-starter' + (isProd ? '-[hash]' : '') + '.css'),
     isProd ? new webpack.optimize.UglifyJsPlugin() : function() {},
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor' + (isProd ? '-[hash]' : '') + '.js'),
     new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV) } })
   ],
   resolveLoader: {
@@ -32,7 +32,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'react-website-starter-[hash].js'
+    filename: 'react-website-starter' + (isProd ? '-[hash]' : '') + '.js'
   },
   devTool: 'eval-source-map',
   devServer: {
