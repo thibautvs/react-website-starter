53124f03bdfc7c071abc3cdab614a81ccaea6a32
thibaut.vanspaandonck <thibaut@urge2code.com>
2016-06-07 09:23:53 +0200

    Misc webpack config improvements

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var isProd = process.env.NODE_ENV === 'production';
+var isDev = !isProd;
 
 module.exports = {
   context: path.resolve(__dirname, 'app'),
@@ -13,8 +14,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.scss$/, loader: isProd ? ExtractTextPlugin.extract('style', 'css!sass!postcss') : 'style!css?sourceMap!sass?sourceMap!postcss' },
-      { test: /\.js$/, exclude: /node_modules/, loader: (isProd ? '' : 'react-hot!') + 'babel?presets[]=es2015&presets[]=react' },
+      { test: /\.scss$/, loader: isDev ? 'style!css?sourceMap!sass?sourceMap!postcss' : ExtractTextPlugin.extract('style', 'css!sass!postcss') },
+      { test: /\.js$/, exclude: /node_modules/, loader: (isDev ? 'react-hot!' : '') + 'babel?presets[]=es2015&presets[]=react' },
       { test: /assets/, loader: 'file?name=[path][name].[ext]' },
       { test: require.resolve('react'), loader: 'expose?React' }
     ],
@@ -22,9 +23,9 @@ module.exports = {
   postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
   plugins: [
     new HtmlWebpackPlugin({ template: './index.tmpl.html' }),
-    new ExtractTextPlugin('react-website-starter' + (isProd ? '-[hash]' : '') + '.css'),
-    isProd ? new webpack.optimize.UglifyJsPlugin() : function() {},
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor' + (isProd ? '-[hash]' : '') + '.js'),
+    isProd ? new ExtractTextPlugin('react-website-starter' + (isProd ? '-[hash]' : '') + '.css') : function() {},
+    isProd ? new webpack.optimize.UglifyJsPlugin() : function() {},
     new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV) } })
   ],
   resolveLoader: {
